1ddfe48142d60c628dca7f6520f15ff776421de3
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-03 12:59:27 -0600

    Loaders de imágenes

--- js-portfolio/webpack.config.js
+++ js-portfolio/webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
           'css-loader',
           'stylus-loader'
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   },
