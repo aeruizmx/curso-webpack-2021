44aab5b48b96be8151ce6c0fdac1c09bb356f7b4
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-03 12:45:06 -0600

    Loaders para CSS y preprocesadores de CSS

--- js-portfolio/webpack.config.js
+++ js-portfolio/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // Entry nos permite decir el punto de entrada de nuestra aplicación
@@ -24,6 +25,14 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ]
       }
     ]
   },
@@ -32,6 +41,7 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 }
